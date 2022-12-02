1b5cb2f081d98f2d955f083926d30c5b625b9842
Alibanayeean <98310529+Alibanayeean@users.noreply.github.com>
2022-12-02 18:56:14 +0330

    Third commit in feature/my-new-feature

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First Test");
         System.out.println("Second Test");
-        System.out.println("Second Test");
+        System.out.println("Third Test");
     }
 }
